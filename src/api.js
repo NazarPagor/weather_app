bc8dda9bec5f896c3fdad6e6b8a120136d1bafa4
Nazar <nazarpahor@intercode.io>
2023-05-29 17:54:06 +0300

    Initial commit

---
+++ src/api.js
@@ -0,0 +1,8 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "4d549806a9msh5195078fd9ccbebp14a815jsne289a2b31a8a",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
---
+++ src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org
+module.exports = {
+  key: "8cac1f1af63c31dde525d6588262bb75",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
